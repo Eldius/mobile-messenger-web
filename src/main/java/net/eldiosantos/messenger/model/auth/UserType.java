472383a07a781c732fd46a1b049f985177504fe5
Eldius <eldiosantos@gmail.com>
2015-03-07 12:03:14 -0300

    Adicao de enum para diferenciar os niveis de acesso.
    Signed-off-by: Eldius <eldiosantos@gmail.com>

---
+++ src/main/java/net/eldiosantos/messenger/model/auth/UserType.java
@@ -0,0 +1,15 @@
+package net.eldiosantos.messenger.model.auth;
+
+/**
+ * Created by Eldius on 06/03/2015.
+ */
+public enum UserType {
+    ADMIN(0)
+    , USER(100);
+
+    private final int accessLevel;
+
+    UserType(final int accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+}
