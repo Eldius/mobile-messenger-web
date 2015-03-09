9f4a60011a0818bd0409dc891d5ec20b5d0a4d9f
Eldius <eldiosantos@gmail.com>
2015-03-09 16:13:54 -0300

    Criacao de handler para redirecionar para a pagina inicial em caso de as inscricoes nao estarem abertas.

--- src/main/java/net/eldiosantos/messenger/rule/OpenregistrationRule.java
+++ src/main/java/net/eldiosantos/messenger/rule/OpenregistrationRule.java
@@ -1,14 +1,17 @@
 package net.eldiosantos.messenger.rule;
 
+import br.com.caelum.brutauth.auth.annotations.HandledBy;
 import br.com.caelum.brutauth.auth.rules.SimpleBrutauthRule;
 import net.eldiosantos.messenger.model.config.ServerConfig;
 import net.eldiosantos.messenger.properties.ServerConfigGetter;
+import net.eldiosantos.messenger.rule.handler.OpenRegistrationHandler;
 
 import javax.inject.Inject;
 
 /**
  * Created by eldio.junior on 09/03/2015.
  */
+@HandledBy(OpenRegistrationHandler.class)
 public class OpenregistrationRule implements SimpleBrutauthRule {
 
     @Inject
