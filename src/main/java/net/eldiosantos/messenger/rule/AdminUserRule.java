56f7eee3e7c4d67a45a7102ae00dc89bb89d8fe5
Eldius <eldiosantos@gmail.com>
2015-03-09 18:03:00 -0300

    Inclusao de validacao de perfil de acesso para o menu Admin.

---
+++ src/main/java/net/eldiosantos/messenger/rule/AdminUserRule.java
@@ -0,0 +1,21 @@
+package net.eldiosantos.messenger.rule;
+
+import br.com.caelum.brutauth.auth.rules.SimpleBrutauthRule;
+import net.eldiosantos.messenger.auth.UserSession;
+import net.eldiosantos.messenger.model.auth.UserType;
+
+import javax.inject.Inject;
+
+/**
+ * Created by eldio.junior on 09/03/2015.
+ */
+public class AdminUserRule implements SimpleBrutauthRule {
+
+    @Inject
+    private UserSession userSession;
+
+    @Override
+    public boolean isAllowed(long accessLevel) {
+        return (userSession.getUser()!=null? UserType.ADMIN.equals(userSession.getUser().getUserType()):false);
+    }
+}
